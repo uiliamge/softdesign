aed5ed3b51d8ef3068cd537ab332e18b4097592f
Uiliam Goltz Elesbão <uiliamge@gmail.com>
2023-12-19 15:35:13 -0300

    Estrutura do projeto.

---
+++ Domain/Entities/Book.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Book
+    {
+        [Key]
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
