3bdb113163749a0c2f7f36b03e0da4f99f805725
Uiliam Goltz Elesbão <uiliamge@gmail.com>
2023-12-21 01:32:52 -0300

    Criação da migration de criação do banco.

--- Infrastructure/BooksDbContext.cs
+++ Infrastructure/BooksDbContext.cs
@@ -1,15 +1,14 @@
 ﻿using Domain;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure
 {
     public class BooksDbContext : DbContext
     {
+        public BooksDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public virtual DbSet<Book> Books { get; set; }
     }
 }
---
+++ Infrastructure/Migrations/20231221042920_Inicializacao.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Inicializacao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReleaseYear = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
